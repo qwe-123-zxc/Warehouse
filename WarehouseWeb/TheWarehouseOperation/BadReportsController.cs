eeac7b6678e511ccc9c7972cf5f2d0a74f1deacc
端祥 陈 <2642527692@qq.com>
2020-04-08 12:47:10 +0800

    陈端祥：仓库作业页面

---
+++ WarehouseWeb/TheWarehouseOperation/BadReportsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WarehouseWeb.TheWarehouseOperation
+{
+    public class BadReportsController : Controller
+    {
+        /// <summary>
+        /// 报损管理
+        /// </summary>
+        /// <returns></returns>
+        // GET: BadReports
+        public ActionResult ListBadReport()
+        {
+            return View();
+        }
+
+        public ActionResult ListAdd()
+        {
+            return View();
+        }
+    }
+}
