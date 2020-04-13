ee7c7db21e1d05bd4402dc98005f355dda251136
32840 <32840@LAPTOP-GJIKMKCC>
2020-04-13 22:24:09 +0800

    txx

---
+++ WarehouseBLL/Admin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+using Models;
+using WarehouseDAL;
+namespace WarehouseBLL
+{
+    public class Admin:BaseManager<Admin>
+    {
+    }
+}
--- WarehouseWeb/TheWarehouseOperation/BadReportsController.cs
+++ WarehouseWeb/TheWarehouseOperation/BadReportsController.cs
@@ -27,6 +27,5 @@ namespace WarehouseWeb.TheWarehouseOperation
         {
             return View();
         }
-           
     }
 }
