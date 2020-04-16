cb57d4d84c3d8d2957e74bed4307b1462e9c631b
端祥 陈 <chen2642527692@hotmail.com>
2020-04-16 16:30:50 +0800

    1

--- WarehouseWeb/TheWarehouseOperation/InStoragesController.cs
+++ WarehouseWeb/TheWarehouseOperation/InStoragesController.cs
@@ -6,55 +6,18 @@ using System.Web.Mvc;
 using Models;
 using WarehouseBLL;
 using System.Linq.Expressions;
-using WarehouseWeb.Models.Dto;
 
 namespace WarehouseWeb.TheWarehouseOperation
 {
-    public class InStoragesController : QueryBaseControllers
+    public class InStoragesController : System.Web.Mvc.Controller
     {
         /// <summary>
         /// 入库管理
         /// </summary>
         /// <returns></returns>
         // GET: InStorages
-        public ActionResult List(QueryInStorageDto dto)
+        public ActionResult List()
         {
-            Expression<Func<InStorage, bool>> where = item => item.AuditTime >= dto.State && item.AuditTime <= dto.End;
-            if (!string.IsNullOrEmpty(dto.InSNum))
-            {
-                where = where.And(i => i.InSNum.IndexOf(dto.InSNum) != -1);
-            }
-            if (dto.InSTypeId!=9999)
-            {
-                where = where.And(i => i.InSTypeId == dto.InSTypeId);
-            }
-            if (dto.SupplierId != 99999999)
-            {
-                where = where.And(i => i.SupplierId == dto.SupplierId);
-            }
-
-            if (!string.IsNullOrEmpty(dto.dsh))
-            {
-                where = where.And(i => i.Status == dto.dsh);
-            }
-
-            if (!string.IsNullOrEmpty(dto.shtg))
-            {
-                where = where.And(i => i.Status == dto.shtg);
-            }
-
-            if (!string.IsNullOrEmpty(dto.shsb))
-            {
-                where = where.And(i => i.Status == dto.shsb);
-            }
-            //查询所有
-            GetRequestPage(dto);
-            var pageIndex = dto.PageIndex;
-            var count = 0;
-            var pageCount = 0;
-            var listInfo = new InStorageManager();
-            var info = listInfo.GetByWhereDesc(where,i=>i.AuditTime,ref pageIndex,ref count,ref pageCount,2);
-
             //供应商
             var GonYinShang = new SupplierManager();
             var gys = GonYinShang.GetAll();
@@ -66,14 +29,46 @@ namespace WarehouseWeb.TheWarehouseOperation
             lty.Insert(0, new InStorageType() { Id = 9999, InSTypeName = "请选择入库单类型" });
             ViewBag.InSTypeId = new SelectList(lty, "Id", "InSTypeName");
 
-            ViewBag.PageIndex = pageIndex;
-            ViewBag.Count = count;
-            ViewBag.PageCount = pageCount;
-            ViewBag.lisInfo = info;
-            ViewBag.dto1 = dto;
             return View();
         }
-        
+
+        public ActionResult ListAjax(string zt, string InSNum, string state, string end, int SupplierId, int InSTypeId, int pageIndex)
+        {
+            var stateDate = Convert.ToDateTime(state);
+            var endDate = Convert.ToDateTime(end);
+            var inStorage = new InStorageManager();
+            Expression<Func<InStorage, bool>> where = i => i.AuditTime >= stateDate && i.AuditTime <= endDate;
+            if (!string.IsNullOrEmpty(zt))
+            {
+                where = where.And(i => i.Status == zt);
+            }
+            if (!string.IsNullOrEmpty(InSNum))
+            {
+                where = where.And(i => i.InSNum.IndexOf(InSNum) != -1);
+            }
+            if (SupplierId != 99999999)
+            {
+                where = where.And(i => i.SupplierId == SupplierId);
+            }
+            if (InSTypeId != 9999)
+            {
+                where = where.And(i => i.InSTypeId == InSTypeId);
+            }
+            var pageCount = 0;
+            var count = 0;
+            var s = inStorage.GetByWhereDesc(where, item => item.AuditTime, ref pageIndex, ref count, ref pageCount, 2);
+            //格式转换
+            var newFormatList = s.Select(i => new { InSNum = i.InSNum, InSTypeId = i.InSTypeId, SupplierId = i.SupplierId, Num = i.Num, SumMoney = i.SumMoney, Status = i.Status, AuditUser = i.AuditUser, AuditTime = i.AuditTime.ToString("yyyy-MM-dd") });
+            var result = new
+            {
+                PageCount = pageCount,
+                Count = count,
+                PageIndex = pageIndex,
+                InstorageInfo = newFormatList
+            };
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
         public ActionResult ListAdd()
         {
             return View();
