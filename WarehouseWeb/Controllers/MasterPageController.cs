8beeb192c6faaca40224c8bba86365c2053e9cd0
13957 <13957@>
2020-04-16 14:30:43 +0800

    lht0416

--- WarehouseWeb/Controllers/MasterPageController.cs
+++ WarehouseWeb/Controllers/MasterPageController.cs
@@ -24,26 +24,10 @@ namespace WarehouseWeb.Controllers
         FunctionManager functionManager = new FunctionManager();
         AdminManger adminManger = new AdminManger();
         RolePowerManager rolePowerManager = new RolePowerManager();
+        RoleManeger roleManeger = new RoleManeger();
+        
 
-        public ActionResult UserAndPwd(string userName, string pwd)
-        {
-            Admin admin = adminManger.GetByWhere(item => item.UserName == userName && item.Password == pwd).SingleOrDefault();
-            if (admin != null)
-            {
-                var result = new
-                {
-                    user = admin,
-                    msg= "登陆成功"
-                };
-                return Json(result, JsonRequestBehavior.AllowGet);
-            }
-            else
-            {
-                return Json("登陆失败", JsonRequestBehavior.AllowGet);
-            }
-        }
-
-        //侧边栏
+        //侧边栏——判断用户是否存在
         public ActionResult GetSidebarData(string userName, string pwd)
         {
             Admin admin = adminManger.GetByWhere(item => item.UserName == userName && item.Password == pwd).SingleOrDefault();
@@ -51,7 +35,6 @@ namespace WarehouseWeb.Controllers
             {
                 admin = adminManger.GetByWhere(item => item.UserName == userName).SingleOrDefault();
             }
-            this.HttpContext.Session["userName"] = admin.UserName ;
             if (admin != null)
             {
                 List<Function> list = functionManager.GetByLinq(admin.RoleId);
@@ -62,11 +45,74 @@ namespace WarehouseWeb.Controllers
                     rootMeun = rootMeun,
                     msg="登陆成功"
                 };
+                this.HttpContext.Session["userName"] = admin.UserName;
                 return Json(newList, JsonRequestBehavior.AllowGet);
             }
             else
             {
                 return Json("登陆失败", JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        //查询个人信息
+        public ActionResult QueryPersonal(string username)
+        {
+            Admin admin = adminManger.GetByWhere(item => item.UserName == username).SingleOrDefault();
+            Role roles = roleManeger.GetByWhere(item => item.Id == admin.RoleId).SingleOrDefault();
+            var newAdmin = new
+            {
+               userName = admin.UserName,
+               userCode = admin.UserCode,
+               realName = admin.RealName,
+               email = admin.Email,
+               phone=admin.Phone,
+               role=roles.RoleName
+            };
+            var result = new
+            {
+                adminData = newAdmin
+            };
+            return Json(result,JsonRequestBehavior.AllowGet);
+        }
+
+        //修改个人信息
+        public ActionResult UpdatePersonal(Admin admin,string userName)
+        {
+            Admin admins = adminManger.GetByWhere(item => item.UserName == userName ).SingleOrDefault();
+            admins.Phone = admin.Phone;
+            admins.RealName = admin.RealName;
+            admins.Email = admin.Email;
+            bool val = adminManger.Update(admins);
+            if (val)
+            {
+                return Json("修改成功", JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json("修改失败", JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        //修改密码
+        public ActionResult UpdatePwd(string passWord, string pwd, string userName)
+        {
+            Admin admin = adminManger.GetByWhere(item => item.UserName == userName).SingleOrDefault();
+            if (admin.Password.Equals(passWord))
+            {
+                admin.Password = pwd;
+                bool val = adminManger.Update(admin);
+                if (val)
+                {
+                    return Json("修改成功", JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    return Json("修改失败", JsonRequestBehavior.AllowGet);
+                }
+            }
+            else
+            {
+                return Json("当前密码错误", JsonRequestBehavior.AllowGet);
             }
         }
     }
